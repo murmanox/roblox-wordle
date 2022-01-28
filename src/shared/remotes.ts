60985552a1348b121c5720e46da1fa1f63df9697
Murmanox <murmanox@hotmail.com>
2022-01-28 20:22:40 +0000

    added documentation

--- src/shared/remotes.ts
+++ src/shared/remotes.ts
@@ -2,8 +2,11 @@ import { Definitions } from '@rbxts/net'
 import { IGuessResponse } from 'types/interfaces/network-types'
 
 const remotes = Definitions.Create({
+	/** Checks if the guessed word matches the word saved on the server. */
 	guessWord: Definitions.ServerAsyncFunction<(word: string) => IGuessResponse>(),
-	getRandomWord: Definitions.ServerAsyncFunction<(length: number) => string>(),
+
+	/** Give the player a new word to guess */
+	getRandomWord: Definitions.ClientToServerEvent<[length: number]>(),
 })
 
 export = remotes
