e7bb72332ad143053b62a03bcb07ac8a841a070d
Murmanox <murmanox@hotmail.com>
2022-01-28 17:46:52 +0000

    initial commit

--- src/server/main.server.ts
+++ src/server/main.server.ts
@@ -1,14 +1,3 @@
 import { makeHello } from 'shared/module'
 
 print(makeHello('main.server.ts'))
-
-const foo_bar = 'bar'
-
-const x = {
-	foo: 1,
-	bar: 2,
-	hello: 'string',
-	oncomplete: () => {
-		return 2
-	},
-}
