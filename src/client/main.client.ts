6a0a7d37a8cb7a7e9cdf4cafec9ce0bffe8dcd73
Murmanox <murmanox@hotmail.com>
2022-01-29 07:34:00 +0000

    converted wrapper to async function

--- src/client/main.client.ts
+++ src/client/main.client.ts
@@ -1,14 +1,11 @@
 import remotes from 'shared/remotes'
 
 // Wrapper for remote to print error
-const guess = (str: string) => {
-	return remotes.Client.Get('guessWord')
-		.CallServerAsync(str, 777)
-		.then((response) => {
-			if (!response.success) {
-				print(response.error)
-			}
-		})
+const guess = async (str: string) => {
+	const response = await remotes.Client.Get('guessWord').CallServerAsync(str, 777)
+	if (!response.success) {
+		print(response.error)
+	}
 }
 
 remotes.Client.Get('getRandomWord')
