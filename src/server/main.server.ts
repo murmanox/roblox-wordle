7d800c5abe7ff30334f00cdf8caf31f099ceabbc
Murmanox <murmanox@hotmail.com>
2022-01-29 14:30:49 +0000

    getWord returns player's current word

--- src/server/main.server.ts
+++ src/server/main.server.ts
@@ -1,7 +1,43 @@
+import { Players } from '@rbxts/services'
 import remotes from 'shared/remotes'
 import { IPlayerGuessData } from 'types/interfaces/guess-types'
+import { IWordResponse } from 'types/interfaces/network-types'
 import { playerWordMap } from '../shared/playerWordMap'
 import { analyzeGuess } from './word-match'
+
+function getWord(player: Player, length: number): IWordResponse {
+	const wordData = playerWordMap.get(player)
+
+	// Player already has a word to guess
+	if (wordData) {
+		const { word, guessCount, previousGuesses } = wordData
+
+		return {
+			length: word.size(),
+			guessCount,
+			previousGuesses,
+		}
+	}
+
+	// Get random word using length
+	const newWord = 'weary'
+
+	// Reset player's guesses and assign new word to them
+	const guessData: IPlayerGuessData = {
+		word: newWord,
+		guessCount: 0,
+		previousGuesses: [],
+	}
+
+	playerWordMap.set(player, guessData)
+
+	// Return the length of the new word
+	return {
+		length: newWord.size(),
+		guessCount: guessData.guessCount,
+		previousGuesses: guessData.previousGuesses,
+	}
+}
 
 remotes.Server.Create('guessWord').SetCallback((player, guess) => {
 	const playerGuessData = playerWordMap.get(player)!
@@ -20,21 +56,22 @@ remotes.Server.Create('guessWord').SetCallback((player, guess) => {
 	return response
 })
 
-remotes.Server.Create('getRandomWord').SetCallback((player) => {
-	// Get random word
-	const newWord = 'weary'
+remotes.Server.Create('getWord').SetCallback(getWord)
 
-	print(`${player}'s new word is ${newWord}`)
-
-	// Reset player's guesses and assign new word to them
-	const guessData: IPlayerGuessData = {
-		word: newWord,
+Players.PlayerAdded.Connect((player) => {
+	// Load player data from ProfileService
+	const tempData: IPlayerGuessData = {
+		// NOT DRY!!!!
+		word: 'wordy',
 		guessCount: 0,
 		previousGuesses: [],
 	}
 
-	playerWordMap.set(player, guessData)
+	// Save into word map
+	playerWordMap.set(player, tempData)
+})
 
-	// Return the length of the new word
-	return newWord.size()
+Players.PlayerRemoving.Connect((player) => {
+	// Delete data related to player when they leave
+	playerWordMap.delete(player)
 })
