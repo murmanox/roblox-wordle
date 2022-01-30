00549ed43685da3512457b72935e53dd228a4099
Murmanox <murmanox@hotmail.com>
2022-01-30 19:34:39 +0000

    removed playerWordMap, use DataManager instead

--- src/server/main.server.ts
+++ src/server/main.server.ts
@@ -1,22 +1,20 @@
 import { Players } from '@rbxts/services'
 import remotes from 'shared/remotes'
-import { IPlayerGuessData } from 'types/interfaces/guess-types'
 import { IWordResponse } from 'types/interfaces/network-types'
-import { playerWordMap } from '../shared/playerWordMap'
-import { assignNewWordToPlayer } from './assignNewWordToPlayer'
+import { PlayerProfile } from 'types/interfaces/profile-type'
+import { DataManager } from './profile-service'
+import { randomWord } from './utility/random'
 import { analyzeGuess } from './word-match'
+import withPlayerProfile from './wrappers/withProfile'
 
-function getPlayerProfile(player: Player) {
-	return {
-		word: 'wordy',
-		guessCount: 0,
-		previousGuesses: [],
-	}
-}
-
-function guessWord(player: Player, guess: string) {
-	const playerGuessData = playerWordMap.get(player)!
-	const { word, previousGuesses } = playerGuessData
+/**
+ * Compare the player's guess against their assigned word.
+ * @param player The player that's guessing
+ * @param guess The player's guess
+ */
+function guessWord(player: Player, profile: PlayerProfile, guess: string) {
+	const gameState = profile.Data.gameState
+	const { word, previousGuesses } = gameState
 
 	print(`${player} guessed ${guess}`)
 
@@ -27,8 +25,8 @@ function guessWord(player: Player, guess: string) {
 	}
 
 	// Increment the amount of guesses by this player
-	playerGuessData.guessCount += 1
-	playerGuessData.previousGuesses.push(guess)
+	gameState.guessCount += 1
+	gameState.previousGuesses.push(guess)
 
 	// Player has won
 	if (response.win) {
@@ -39,14 +37,18 @@ function guessWord(player: Player, guess: string) {
 	return response
 }
 
-function getWord(player: Player, length: number): IWordResponse {
-	const wordData = playerWordMap.get(player)
+/**
+ * Get information about the player's current word. If they don't have a word, select a new one.
+ * @param player The player whose word is retrieved
+ * @param length The desired length of the word if a new word is selected
+ */
+function getWord(player: Player, profile: PlayerProfile, length: number): IWordResponse {
+	const { word, guessCount, previousGuesses } = profile.Data.gameState
 
 	// Player already has a word to guess
-	if (wordData) {
-		const { word, guessCount, previousGuesses } = wordData
-
+	if (word !== '') {
 		return {
+			success: true,
 			length: word.size(),
 			guessCount,
 			previousGuesses,
@@ -54,24 +56,19 @@ function getWord(player: Player, length: number): IWordResponse {
 	}
 
 	// Reset player's guesses and assign new word to them
-	const word = assignNewWordToPlayer(player, length)
+	const newWord = randomWord(length)
+	DataManager.setPlayerWord(player, newWord)
 
 	// Return the length of the new word
-	return { length: word.size(), guessCount: 0, previousGuesses: [] }
+	return { success: true, length: newWord.size(), guessCount: 0, previousGuesses: [] }
 }
 
 Players.PlayerAdded.Connect((player) => {
-	// Load player data from ProfileService
-	const tempData: IPlayerGuessData = getPlayerProfile(player)
-
-	// Save into word map
-	playerWordMap.set(player, tempData)
+	DataManager.getProfileAsync(player).then((profile) =>
+		print(`Word assigned: ${profile.Data.gameState.word}`)
+	)
 })
 
-Players.PlayerRemoving.Connect((player) => {
-	// Delete data related to player when they leave
-	playerWordMap.delete(player)
-})
-
-remotes.Server.Create('guessWord').SetCallback(guessWord)
-remotes.Server.Create('getWord').SetCallback(getWord)
+// Add callbacks to server events
+remotes.Server.Create('guessWord').SetCallback(withPlayerProfile(guessWord))
+remotes.Server.Create('getWord').SetCallback(withPlayerProfile(getWord))
