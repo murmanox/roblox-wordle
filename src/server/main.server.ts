3a6fdf57f42711f39664c2d39f81a2be599dc473
Murmanox <murmanox@hotmail.com>
2022-01-30 06:57:03 +0000

    converted events to separate functions

--- src/server/main.server.ts
+++ src/server/main.server.ts
@@ -3,8 +3,41 @@ import remotes from 'shared/remotes'
 import { IPlayerGuessData } from 'types/interfaces/guess-types'
 import { IWordResponse } from 'types/interfaces/network-types'
 import { playerWordMap } from '../shared/playerWordMap'
-import { randomWord } from './utility/random'
+import { assignNewWordToPlayer } from './assignNewWordToPlayer'
 import { analyzeGuess } from './word-match'
+
+function getPlayerProfile(player: Player) {
+	return {
+		word: 'wordy',
+		guessCount: 0,
+		previousGuesses: [],
+	}
+}
+
+function guessWord(player: Player, guess: string) {
+	const playerGuessData = playerWordMap.get(player)!
+	const { word, previousGuesses } = playerGuessData
+
+	print(`${player} guessed ${guess}`)
+
+	const response = analyzeGuess(guess, word, previousGuesses)
+
+	if (!response.success) {
+		return response
+	}
+
+	// Increment the amount of guesses by this player
+	playerGuessData.guessCount += 1
+	playerGuessData.previousGuesses.push(guess)
+
+	// Player has won
+	if (response.win) {
+		// Give points
+		// Assign player new word
+	}
+
+	return response
+}
 
 function getWord(player: Player, length: number): IWordResponse {
 	const wordData = playerWordMap.get(player)
@@ -21,49 +54,15 @@ function getWord(player: Player, length: number): IWordResponse {
 	}
 
 	// Reset player's guesses and assign new word to them
-	const guessData: IPlayerGuessData = {
-		word: randomWord(length),
-		guessCount: 0,
-		previousGuesses: [],
-	}
-
-	playerWordMap.set(player, guessData)
+	const word = assignNewWordToPlayer(player, length)
 
 	// Return the length of the new word
-	return {
-		length: guessData.word.size(),
-		guessCount: guessData.guessCount,
-		previousGuesses: guessData.previousGuesses,
-	}
+	return { length: word.size(), guessCount: 0, previousGuesses: [] }
 }
-
-remotes.Server.Create('guessWord').SetCallback((player, guess) => {
-	const playerGuessData = playerWordMap.get(player)!
-	const { word, previousGuesses } = playerGuessData
-
-	print(`${player} guessed ${guess}`)
-
-	const response = analyzeGuess(guess, word, previousGuesses)
-
-	// Increment the amount of guesses by this player
-	if (response.success) {
-		playerGuessData.guessCount += 1
-		playerGuessData.previousGuesses.push(guess)
-	}
-
-	return response
-})
-
-remotes.Server.Create('getWord').SetCallback(getWord)
 
 Players.PlayerAdded.Connect((player) => {
 	// Load player data from ProfileService
-	const tempData: IPlayerGuessData = {
-		// NOT DRY!!!!
-		word: 'wordy',
-		guessCount: 0,
-		previousGuesses: [],
-	}
+	const tempData: IPlayerGuessData = getPlayerProfile(player)
 
 	// Save into word map
 	playerWordMap.set(player, tempData)
@@ -73,3 +72,6 @@ Players.PlayerRemoving.Connect((player) => {
 	// Delete data related to player when they leave
 	playerWordMap.delete(player)
 })
+
+remotes.Server.Create('guessWord').SetCallback(guessWord)
+remotes.Server.Create('getWord').SetCallback(getWord)
