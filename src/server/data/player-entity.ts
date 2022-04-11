f42a782033d737746cc4b8a0e18d080a73eba962
Murmanox <murmanox@hotmail.com>
2022-04-11 08:21:56 +0100

    added documentation

--- src/server/data/player-entity.ts
+++ src/server/data/player-entity.ts
@@ -3,8 +3,14 @@ import { DeepPartial } from 'types/util/partial'
 import { DeepReadonly } from 'types/util/readonly'
 import { PlayerProfile, PlayerProfileData } from './player-data'
 
+/**
+ * Container for data relating to the player.
+ * Provides an API to update the player's ProfileService profile.
+ */
 export class PlayerEntity {
 	public readonly name: string
+
+	/** Readonly version of the player's profile data. Updates should be done through the `updateData` method. */
 	public data: DeepReadonly<PlayerProfileData>
 
 	constructor(player: Player, private profile: PlayerProfile) {
@@ -12,10 +18,14 @@ export class PlayerEntity {
 		this.data = profile.Data
 	}
 
+	/**
+	 * Updates the player's saved data.
+	 * Nested objects will be merged recursively, so excluded keys will be ignored.
+	 */
 	public updateData(changedData: DeepPartial<PlayerProfileData>) {
 		const newData = Dictionary.mergeDeep(this.data, changedData) as PlayerProfileData
 
-		print('Data updated from', this.data, 'to', newData)
+		print(`${this.name}'s data updated to`, newData)
 
 		this.profile.Data = newData
 		this.data = newData
--- src/server/data/player-manager.ts
+++ src/server/data/player-manager.ts
@@ -1,10 +1,17 @@
 import { PlayerEntity } from './player-entity'
 import profileLoader from './profile-loader'
 
+/**
+ * Handles the creation of player entities when the player joins the game.
+ * Keeps a record of player entities for external use.
+ */
 class PlayerManager {
 	private playerEntityMap = new Map<Player, PlayerEntity>()
 
-	public async onPlayerJoin(player: Player) {
+	/**
+	 * Creates a PlayerEntity for the passed player
+	 */
+	public async onPlayerJoin(player: Player): Promise<void> {
 		const profile = await profileLoader.loadPlayerProfile(player)
 		if (!profile) return
 
@@ -12,6 +19,9 @@ class PlayerManager {
 		this.playerEntityMap.set(player, entity)
 	}
 
+	/**
+	 * Retrieves the PlayerEntity associated with the passed player
+	 */
 	public getPlayerEntity(player: Player): PlayerEntity | undefined {
 		return this.playerEntityMap.get(player)
 	}
