11771dde1a106a825077cda4af280a8214d56f50
Murmanox <murmanox@hotmail.com>
2022-04-17 20:56:32 +0100

    added simple theme button that mimics roblox UI

--- src/client/interface/components/theme-button/theme-button.tsx
+++ src/client/interface/components/theme-button/theme-button.tsx
@@ -1,38 +1,78 @@
 import Roact from '@rbxts/roact'
-import { hooked } from '@rbxts/roact-hooked'
+import { hooked, Reducer, useReducer } from '@rbxts/roact-hooked'
 import { useSelector, useStore } from '@rbxts/roact-rodux-hooked'
-import { GuiService } from '@rbxts/services'
 import { IClientStore } from 'client/interface/store/rodux'
 
-const inset = GuiService.GetGuiInset()[0]
+type Action =
+	| { type: 'MouseDown' }
+	| { type: 'MouseUp' }
+	| { type: 'MouseEnter' }
+	| { type: 'MouseLeave' }
+
+type State = { color: Color3; transparency: number }
+
+const reducer: Reducer<State, Action> = (state, action) => {
+	switch (action.type) {
+		case 'MouseUp':
+		case 'MouseEnter':
+			return { color: new Color3(1, 1, 1), transparency: 0.9 }
+		case 'MouseDown':
+			return { color: new Color3(), transparency: 0.7 }
+		case 'MouseLeave':
+		default:
+			return { color: new Color3(), transparency: 1 }
+	}
+}
+
+const initialState = { color: new Color3(), transparency: 1 }
 
 interface Props {}
 
 const ThemeButton = hooked<Props>((props) => {
 	const store = useStore<IClientStore>()
-	const state = useSelector((state: IClientStore) => state.theme)
-	const text = state === 'dark' ? '🌛' : '☀️'
+	const theme = useSelector((state: IClientStore) => state.theme)
+
+	const text = theme === 'dark' ? '🌛' : '☀️'
+	const [{ color, transparency }, dispatch] = useReducer(reducer, initialState)
 
 	return (
 		<textbutton
-			Text={text}
+			Text=""
 			Size={UDim2.fromOffset(32, 32)}
-			Position={UDim2.fromOffset(104, 36 - inset.Y)}
+			Position={UDim2.fromOffset(104, 0)}
 			AnchorPoint={new Vector2(0, 1)}
 			BorderSizePixel={0}
-			FontSize={'Size18'}
 			BackgroundTransparency={0.5}
 			BackgroundColor3={new Color3()}
+			AutoButtonColor={false}
 			Event={{
-				Activated: (rbx) => {
+				MouseEnter: () => dispatch({ type: 'MouseEnter' }),
+				MouseLeave: () => dispatch({ type: 'MouseLeave' }),
+				MouseButton1Down: () => dispatch({ type: 'MouseDown' }),
+				MouseButton1Up: () => dispatch({ type: 'MouseUp' }),
+				Activated: () => {
 					store.dispatch({
 						type: 'setTheme',
-						theme: state === 'light' ? 'dark' : 'light',
+						theme: theme === 'light' ? 'dark' : 'light',
 					})
 				},
 			}}
 		>
-			<uipadding PaddingTop={new UDim(0, 2)} PaddingRight={new UDim(0, 1)} />
+			<textlabel
+				FontSize={'Size18'}
+				Text={text}
+				Size={UDim2.fromScale(1, 1)}
+				BackgroundTransparency={1}
+			>
+				<uipadding PaddingTop={new UDim(0, 2)} PaddingRight={new UDim(0, 1)} />
+			</textlabel>
+			<frame
+				Size={UDim2.fromScale(1, 1)}
+				BackgroundColor3={color}
+				BackgroundTransparency={transparency}
+			>
+				<uicorner CornerRadius={new UDim(0.24, 0)} />
+			</frame>
 			<uicorner CornerRadius={new UDim(0.24, 0)} />
 		</textbutton>
 	)
